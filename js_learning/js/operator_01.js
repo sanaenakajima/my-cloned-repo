3dba85f63f9268d8df7a358d9d1ab21dcc8371e9
sanaenakajima <sn8.nakajima@gmail.com>
2024-04-01 16:25:13 +0900

    js課題_算術演算子_課題1_修正

--- js_learning/js/operator_01.js
+++ js_learning/js/operator_01.js
@@ -1,14 +1,8 @@
 //コンソールログ
-let test_1;
-let test_2;
-let test_3;
-let test_4;
-let test_5;
-
-test_1=100;
-test_2=50;
-test_3=40;
-test_4=70;
-test_5=80;
+let test_1=100;
+let test_2=50;
+let test_3=40;
+let test_4=70;
+let test_5=80;
 
 console.log(test_1 + test_2 + test_3 + test_4 + test_5); 
