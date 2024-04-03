f6ca61ce76a8c3396061d4e3d36d67d03aab1b9e
sanaenakajima <sn8.nakajima@gmail.com>
2024-04-03 15:48:14 +0900

    繰り返し処理_課題04

--- js_learning/js/for_03.js
+++ js_learning/js/for_03.js
@@ -1,14 +1,14 @@
-const participants = [
+const participant = [
     { name: 'taro', age: 20 },
     { name: 'hanako', age: 15 },
     { name: 'john', age: 25 }
   ];
   
-  for (let i = 0; i < participants.length; i++) {
-    const participant = participants[i];
-    if (participant.age >= 18) {
-      console.log(`${participant.name}さんは18歳以上のため参加可能です`);
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i].age >= 18) {
+      console.log(`${participant[i].name}さんは18歳以上のため参加可能です`);
     } else {
-      console.log(`${participant.name}さんは18歳未満のため参加できません`);
+      console.log(`${participant[i].name}さんは18歳未満のため参加できません`);
     }
   }
+  
