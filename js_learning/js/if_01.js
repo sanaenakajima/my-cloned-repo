15da4182e5a3f70698717495faf861de507820c1
sanaenakajima <sn8.nakajima@gmail.com>
2024-04-03 11:55:45 +0900

    条件分岐_課題01_修正

--- js_learning/js/if_01.js
+++ js_learning/js/if_01.js
@@ -1,23 +1,23 @@
 
 const button = document.getElementById("button");
 const num = document.getElementById("num");
-const hantei = document.getElementById("hantei");
+const judgement = document.getElementById("judgement");
 
 button.addEventListener("click",function() {
 
-  if(num.value > 89 && num.value <= 100) {
-    console.log(hantei.textContent="「あなたの成績は秀です」");
+  if(num.value >= 90 && num.value <= 100) {
+    console.log(judgement.textContent="「あなたの成績は秀です」");
   
-  } else if (num.value > 79 && num.value < 90) {
-    console.log(hantei.textContent="「あなたの成績は優です」");
+  } else if (num.value >= 80 && num.value <= 89) {
+    console.log(judgement.textContent="「あなたの成績は優です」");
   
-  } else if (num.value >69 && num.value < 80) {
-    console.log(hantei.textContent="「あなたの成績は良です」");
+  } else if (num.value >= 70 && num.value <= 79) {
+    console.log(judgement.textContent="「あなたの成績は良です」");
   
-  } else if (num.value > 59 && num.value < 70) {
-    console.log(hantei.textContent="「あなたの成績は可です」");
+  } else if (num.value >= 60 && num.value <= 69) {
+    console.log(judgement.textContent="「あなたの成績は可です」");
   
-  } else if (num.value < 60) {
-    console.log(hantei.textContent="「あなたの成績は不可です」");
+  } else if (num.value <= 59) {
+    console.log(judgement.textContent="「あなたの成績は不可です」");
   };
 });
