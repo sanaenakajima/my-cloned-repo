042157eb21e3d5588581152f2a067cb6e8f48521
sanaenakajima <sn8.nakajima@gmail.com>
2024-04-03 15:55:20 +0900

    繰り返し処理_課題01

--- js_learning/js/for_01.js
+++ js_learning/js/for_01.js
@@ -1,11 +1,7 @@
-function printMultiplesOfThree() {
     for (let i = 0; i <= 100; i++) {
       if (i !== 0 && i % 3 === 0) {
-        console.log(`${i}: 3の倍数です`);
+        console.log(`3の倍数です`);
       } else {
         console.log(i);
       }
     }
-  }
-  
-  printMultiplesOfThree();
