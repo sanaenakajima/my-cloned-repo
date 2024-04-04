8f784c79e017e4b101ad305f4f56ed1142627f63
sanaenakajima <sn8.nakajima@gmail.com>
2024-04-04 17:35:11 +0900

    classとオブジェクト指向課題01

--- js_learning/js/class_01.js
+++ js_learning/js/class_01.js
@@ -1,3 +1,6 @@
+
+//'Character'クラスは、キャラクターを表すオブジェクトを作成するためのクラス 
+//'constructor(name, rank)'メソッドは、キャラクターの名前とランクを受け取り、新しいキャラクターオブジェクトを作成
 class Character {
     constructor(name, rank) {
         this.name = name;
@@ -5,32 +8,44 @@ class Character {
     }
 }
 
+//'Gacha'クラスは、ガチャを表すオブジェクトを作成するためのクラス
+//'constructor(characters)'メソッドは、キャラクターの配列を受け取り、新しいガチャオブジェクトを作成.drawCountを初期化
 class Gacha {
     constructor(characters) {
         this.characters = characters;
-        this.drawCount = 0;
+        this.drawCount = 1;
     }
 
+//'drawCharacter()'メソッドは、ガチャからキャラクターをランダムに引くメソッド
     drawCharacter() {
         const randomIndex = Math.floor(Math.random() * this.characters.length);
         const drawnCharacter = this.characters[randomIndex];
         return drawnCharacter;
     }
-
+//'draw()'メソッドは、'drawCharacter()'を呼び出してキャラクターを引き、引いたキャラクターと引いた回数をコンソールに表示
     draw() {
         const result = this.drawCharacter();
-        console.log("引いたキャラ:", result);
-        this.drawCount++;
-        console.log("引いた回数:", this.drawCount);
-    }
+    console.log(
+        "ガチャの結果は・・・ランク" + result.rank + "の" + result.name + "が当たりました！\n" +
+        "現在ガチャを" + this.drawCount + "回引きました"
+    );
+    this.drawCount++;
+}
 }
 
 // キャラクターのリスト
 const characters = [
-    new Character("キャラA", "A"),
-    new Character("キャラB", "B"),
-    new Character("キャラC", "C")
+    new Character("キャラA", getRandomRank()),
+    new Character("キャラB", getRandomRank()),
+    new Character("キャラC", getRandomRank())
 ];
+
+// ランダムなランクを生成する関数
+function getRandomRank() {
+    const ranks = ['A', 'B', 'C'];
+    const randomIndex = Math.floor(Math.random() * ranks.length);
+    return ranks[randomIndex];
+}
 
 // ガチャのインスタンスを作成
 const gacha = new Gacha(characters);
